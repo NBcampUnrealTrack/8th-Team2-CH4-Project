088cea0d68432ac71a324d9f03b619aab24b0447
Parkchanheyk <parkchanheyk@gmail.com>
2026-06-25 17:46:11 +0900

    Add : 게임모드 추가

---
+++ FieryTale/Source/FieryTale/Public/Core/Gamemode/FTGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "FTGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIERYTALE_API AFTGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
