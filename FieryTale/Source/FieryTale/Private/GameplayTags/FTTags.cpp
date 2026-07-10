35ace4958dd21f575b290f8d924e6053ab296824
Parkchanheyk <parkchanheyk@gmail.com>
2026-07-10 11:59:58 +0900

    ADD : 게임플레이 큐 태그 추가

--- FieryTale/Source/FieryTale/Private/GameplayTags/FTTags.cpp
+++ FieryTale/Source/FieryTale/Private/GameplayTags/FTTags.cpp
@@ -105,7 +105,25 @@ namespace FTTags
     
     namespace FTGameplayCue
     {
-       UE_DEFINE_GAMEPLAY_TAG_COMMENT(Minion_AttackHit, "FTTags.FTGameplayCue.Minion.AttackHit", "미니언 평타 적중 및 투사체 폭발 시 시각/청각 연출 격발용 큐");
-       UE_DEFINE_GAMEPLAY_TAG_COMMENT(Debuff_Stunned, "FTTags.FTGameplayCue.Debuff.Stunned", "기절 상태 지속 시간 동안 머리 위에 루핑 이펙트를 부착하는 상태 큐");
+        UE_DEFINE_GAMEPLAY_TAG_COMMENT(Minion_AttackHit, "FTTags.FTGameplayCue.Minion.AttackHit", "미니언 평타 적중 및 투사체 폭발 시 시각/청각 연출 격발용 큐");
+
+    	// 공용 디버프 
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Debuff_Stunned, "Debuff.Stunned", "기절: 모든 행동이 불가능해지는 상태이상 디버프 태그");
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Debuff_Rooted, "Debuff.Rooted", "속박: 이동이 불가능해지는 상태이상 디버프 태그");
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Debuff_Taunted, "Debuff.Taunted", "도발: 시선이 강제 고정되며 스킬 사용이 불가능해지는 상태이상 디버프 태그");
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Debuff_Slow, "Debuff.Slow", "둔화: 이동 속도가 감소하는 상태이상 디버프 태그");
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Debuff_Knockback, "Debuff.Knockback", "넉백: 중심부에서 바깥쪽으로 밀려나는 상태이상 디버프 태그");
+
+    	// 앨리스 
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Alice_AttackHit, "Event.Alice.AttackHit", "앨리스 기본 공격(로얄 카드) 적중 시 발생하는 이벤트 태그");
+
+    	// 카구야
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Kaguya_AttackHit, "Event.Kaguya.AttackHit", "가구야 기본 공격(죽절격) 적중 시 발생하는 이벤트 태그");
+
+    	// 빨간 망토
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(RedHood_AttackHit, "Event.RedHood.AttackHit", "빨간 망토 기본 공격(사냥꾼의 유산) 적중 시 발생하는 이벤트 태그");
+
+    	// 알라딘
+    	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Aladdin_AttackHit, "Event.Aladdin.AttackHit", "알라딘 기본 공격(램프의 불꽃) 적중 시 발생하는 이벤트 태그");
     }
 }
--- FieryTale/Source/FieryTale/Public/GameplayTags/FTTags.h
+++ FieryTale/Source/FieryTale/Public/GameplayTags/FTTags.h
@@ -107,7 +107,25 @@ namespace FTTags
     
     namespace FTGameplayCue
     {
+        // 미니언 히트
         UE_DECLARE_GAMEPLAY_TAG_EXTERN(Minion_AttackHit);
+    	// 공용 디버프 
         UE_DECLARE_GAMEPLAY_TAG_EXTERN(Debuff_Stunned);
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Debuff_Rooted);
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Debuff_Taunted);
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Debuff_Slow);
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Debuff_Knockback);
+    	
+    	//앨리스 
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Alice_AttackHit);
+    	
+    	//카구야
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Kaguya_AttackHit);
+    	
+    	//빨간 망토
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(RedHood_AttackHit);
+    	
+    	//알라딘
+    	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Aladdin_AttackHit);
     }
 }
